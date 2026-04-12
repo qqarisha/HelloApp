ab026cd277f833835749f86dec3469dfd604d9a2
Завьялова Арина <arina@zavyalov.me>
2026-04-12 15:13:27 +0300

    add migrations

---
+++ HelloApp/Classes/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HelloApp.Classes
+{
+    public class ApplicationContext: DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        public DbSet<Author> Authors { get; set; } = null!;
+
+        public DbSet<Book> Books { get; set; } = null!;
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options): base(options)
+        {
+            
+        }
+    }
+}
