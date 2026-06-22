ab167495e32c2b8624ae5b44102b59073c732f98
Завьялова Арина <arina@zavyalov.me>
2026-06-22 08:47:24 +0300

    add configurations

---
+++ HelloApp/EntityConfigurations/AuthorEntityConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using HelloApp.Classes;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HelloApp.EntityConfigurations
+{
+    public class AuthorEntityConfiguration: IEntityTypeConfiguration<Author>
+    {
+        public void Configure(EntityTypeBuilder<Author> builder)
+        {
+            builder.ToTable("authors");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+        }
+    }
+}
